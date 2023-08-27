9594a484464e0b95fb1fcce130d507ef9d1c9236
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-27 19:40:50 +0000

    Comienzo de la parte visual del componente SecondsCounter

--- src/js/component/SecondsCounter/SecondsCounter.jsx
+++ src/js/component/SecondsCounter/SecondsCounter.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./style.css";
 
-const SecondsCounter = () =>{
-    return <h1>holita mundo</h1>
+const SecondsCounter = ({ seconds }) =>{
+    return (
+        <div className="bigContainer">
+            <div><i class="fa-regular fa-clock"></i></div>
+            <div>0</div>
+            <div>0</div>
+            <div>0</div>
+            <div>0</div>
+            <div>0</div>
+            <div>0</div>
+        </div>
+    )
 }
 
 export default SecondsCounter;
