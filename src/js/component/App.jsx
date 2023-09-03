78fb7b066032619f49a40e8df957d6089341b7b3
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-03 15:51:52 +0000

    Organicé mejor los componentes, ahora todo lo relacionado con el counter está en el mismo lugar SeconsCounter y en el componente App sólo está la instancia del mismo

---
+++ src/js/component/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SecondsCounter from "./component/SecondsCounter"
+
+const App = () => {
+
+  return (
+    <div>
+      <SecondsCounter />
+    </div>
+  );
+};
+
+export default App;
--- src/js/component/SecondsCounter/SecondsCounter.jsx
+++ src/js/component/SecondsCounter/SecondsCounter.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-const SecondsCounter = (props) =>{
+const SecondsCounter = () => {
+    const [counter, setCounter] = useState(0);
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+  
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, []);
+  
+    const six = Math.floor(counter / 100000) % 10;
+    const five = Math.floor(counter / 10000) % 10;
+    const four = Math.floor(counter / 1000) % 10;
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = Math.floor(counter / 1) % 10;
+  
     return (
-        <div className="bigContainer">
-            <div className="relojIcon"><i class="fa-regular fa-clock"></i></div>
-            <div className="sextoDigito">{props.sextoDigito % 10}</div>
-            <div className="quintoDigito">{props.quintoDigito % 10}</div>
-            <div className="cuartoDigito">{props.cuartoDigito % 10}</div>
-            <div className="tercerDigito">{props.tercerDigito % 10}</div>
-            <div className="segundoDigito">{props.segundoDigito % 10}</div>
-            <div className="primerDigito">{props.primerDigito % 10}</div>
+      <div className="bigContainer">
+        <div className="relojIcon">
+          <i className="fa-regular fa-clock"></i>
         </div>
-    )
-}
+        <div className="sextoDigito">{six}</div>
+        <div className="quintoDigito">{five}</div>
+        <div className="cuartoDigito">{four}</div>
+        <div className="tercerDigito">{three}</div>
+        <div className="segundoDigito">{two}</div>
+        <div className="primerDigito">{one}</div>
+      </div>
+    );
+  };
 
 export default SecondsCounter;
